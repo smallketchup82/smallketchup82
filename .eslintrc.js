47db0bc515e2d81bda7137e6ba0c8dd6ec437d0e
smallketchup82 <69545310+smallketchup82@users.noreply.github.com>
2023-04-20 09:45:34 -0400

    Update .eslintrc.js

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,18 +2,14 @@ module.exports = {
 	"env": {
 		"node": true,
 		"commonjs": true,
-		"es2021": true,
-		"browser": true
+		"browser": true,
+		"es6": true
 	},
 	"extends": "eslint:recommended",
 	"parserOptions": {
 		"ecmaVersion": "latest"
 	},
 	"rules": {
-		"indent": [
-			"error",
-			"tab"
-		],
 		"quotes": [
 			"error",
 			"double"
